9b5353a733f53116a29548f5838096f9508e9c2f
djvill <d.vill@pitt.edu>
2022-10-03 17:22:06 -0400

    Minor: Better error msg when in wrong dir for shinytest wrapper

--- APLS Elan File Checker/tests/shinytest.R
+++ APLS Elan File Checker/tests/shinytest.R
@@ -1,3 +1,8 @@
+thisDir <- basename(getwd())
+if (thisDir != "tests") {
+  stop("Working directory must be tests/, not ", thisDir, "/")
+}
+
 library(shinytest)
 # testApp("../", testnames=dir("shinytest/", "test.+R$"))
 testApp("../", testnames=dir("shinytest/", "test-step3.R"))
