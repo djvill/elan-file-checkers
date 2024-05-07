djvill/elan-file-checkers

commit 68dcaadfae6a7ba7c4cadd7535e88c612e3cdccc

Ditch modal box in favor of requiring Transcriber tier; Add AUTHOR attribute for TGs at export (and thus ditch reactiveValues() approach); Add remove_tiers() trs utility

app.R
@@ -28,6 +28,9 @@ neighborhoodRegex <- "^(CB|FH|HD|LV)"
 ##Tier checking
 ##Required non-speaker tiers
 nonSpkrTiers <- c("Comment","Noise","Redaction")
+##Additional required tiers for different file extensions
+addlReqTiers <- list(eaf = character(0L),
+                     textgrid = "Transcriber")
 ##Tiers that should never be present
 prohibTiers <- c("Recheck",
                  "Text",                                                 ##From CLOx
@@ -37,7 +40,7 @@ prohibTiers <- c("Recheck",
 
 ##Dictionary checking
 ##Tiers to exclude from dictionary checking
-noDictCheckTiers <- c("Comment","Noise","Redaction")
+noDictCheckTiers <- c("Comment","Noise","Redaction","Transcriber")
 ##Include local version of aplsDict.txt? Include ONLY local version?
 ##  Only works on Dan's machine, useful for testing new entries without
 ##  committing each time
@@ -52,7 +55,7 @@ caseSens <- FALSE
 
 ##Overlap fixing
 ##Tiers to exclude from overlap checking/fixing
-noOverlapCheckTiers <- c("Comment","Noise")
+noOverlapCheckTiers <- c("Comment","Noise","Transcriber")
 ##Maximum cross-tier misalignment (in ms) to 'snap together'. Set lower to be
 ##  more conservative about what counts as an intended cross-tier alignment
 overlapThresh <- 500
@@ -146,16 +149,24 @@ validateFiles <- function(x, validExts=names(readHandlers)) {
 ## Tiers ==================================================================
 ##Given a transcription object and filename, output character vector of tier
 ##  issues
-tierIssuesOneFile <- function(x, nm, nonSpkrTiers=NULL, prohibTiers=NULL) {
+tierIssuesOneFile <- function(x, nm, 
+                              nonSpkrTiers=NULL, addlReqTiers=NULL, 
+                              prohibTiers=NULL) {
   library(dplyr)
   library(purrr)
   library(stringr)
   
+  ##Check args
   if (!inherits(x, c("trs_transcription", "trs_nesttiers"))) {
     stop("x must be an object of classes trs_transcription and trs_nesttiers")
   }
   if (!is.character(nm) || length(nm) != 1) {
     stop("nm must be a string")
+  }
+  stopifnot(is.list(addlReqTiers))
+  if (any(map_int(addlReqTiers, length) > 1)) {
+    stop("tierIssuesOneFile() can only accept one addlReqTiers tier per ", 
+         "file extension")
   }
   
   ##Get filename, speaker code, neighborhood
@@ -171,12 +182,6 @@ tierIssuesOneFile <- function(x, nm, nonSpkrTiers=NULL, prohibTiers=NULL) {
   ##Initialize empty issues character vector
   issues <- character(0L)
   
-  ##Detect missing/empty AUTHOR attribute
-  author <- attr(x, "AUTHOR")
-  if (is.null(author) || author=="" || tolower(author)=="unspecified") {
-    issues <- c(issues, paste("File missing an AUTHOR attribute"))
-  }
-  
   ##Handle prohibited tiers
   if (!is.null(prohibTiers)) {
     issues <- c(issues,
@@ -188,10 +193,28 @@ tierIssuesOneFile <- function(x, nm, nonSpkrTiers=NULL, prohibTiers=NULL) {
   }
   
   ##Handle missing tiers (non-interviewer)
-  checkTiers <- c(unique(fileInfo$SpkrCode), nonSpkrTiers)
+  fileExt <- tolower(fileInfo$FileExt)
+  addlTiers <- addlReqTiers[[fileExt]]
+  checkTiers <- c(unique(fileInfo$SpkrCode), nonSpkrTiers, addlTiers)
   missingTiers <- setdiff(checkTiers, tierInfo$TIER_ID)
   if (length(missingTiers) > 0) {
     issues <- c(issues, paste("There are no tiers named", missingTiers))
+  }
+  
+  ##Detect missing/empty AUTHOR attribute or empty Transcriber tier contents
+  if (fileExt=="eaf") {
+    author <- attr(x, "AUTHOR")
+    if (is.null(author) || author=="" || tolower(author)=="unspecified") {
+      issues <- c(issues, "File missing an AUTHOR attribute")
+    }
+  } else if (fileExt=="textgrid" && addlTiers %in% tierInfo$TIER_ID) {
+    transcriber <- x[[addlTiers]]$Text
+    if (length(transcriber)==0 || all(transcriber=="")) {
+      issues <- c(issues, paste(addlTiers, "tier is empty"))
+    }
+    if (n_distinct(transcriber) > 1) {
+      issues <- c(issues, paste(addlTiers, "tier has multiple annotations"))
+    }
   }
   
   ##Handle missing interviewer tier
@@ -589,27 +612,9 @@ server <- function(input, output) {
     fileDF
   })
   
-  ##Create reactiveValues() list for author-updating
-  rV <- reactiveValues(trsList = NULL)
-  
   ##Convert valid files to list of trs_transcription objects
-  observeEvent(fileDF(), {
-    anyTG <- 
-      fileDF()$file %>% 
-      map_lgl(~ inherits(.x, "trs_textgrid")) %>% 
-      any()
-    
-    ##If there are any textgrids, prompt for author
-    if (anyTG) {
-      showModal(modalDialog(
-        textInput("author", "Specify transcriber"),
-        span("This will get filled in as the output Elan file's Author ",
-             "attribute"),
-        footer = actionButton("ok", "Submit"))
-      )
-    }
-    
-    rV$trsList <-
+  trsList <- reactive({
+    trsList <-
       fileDF() %>%
       ##As list of files (and/or errors)
       pull(file, name) %>% 
@@ -618,14 +623,8 @@ server <- function(input, output) {
       ##Convert to trs_transcription via method dispatch
       map(~ as.trs_transcription(.x, annotation_metadata=TRUE,
                                  tierAttributes=TRUE))
-  })
-  
-  ##Fill author attribute for textgrids when modal is submitted
-  observeEvent(input$ok, {
-    rV$trsList <- rV$trsList %>% 
-      map_if(~ inherits(.x, "trs_from_textgrid"),
-             ~ add_attributes(.x, list(AUTHOR = input$author)))
-    removeModal()
+    
+    trsList
   })
   
   # Export element for shinytest ---------------------------------------------
@@ -659,9 +658,9 @@ server <- function(input, output) {
       export <- c(export, tagList(pack_val(validationDF, "validationDF")))
     } else {
       ##If step 0 passed, add tier info & at least one fileDF()
-      tierDF <- map(rV$trsList, tier_metadata)
+      tierDF <- map(trsList(), tier_metadata)
       turnsDF <- 
-        rV$trsList %>%
+        trsList() %>%
         map_dfr(~ .x %>% 
                   map_dfr(as_tibble, .id="Tier"),
                 .id="File") %>% 
@@ -676,6 +675,9 @@ server <- function(input, output) {
   
   # Main execution block ----------------------------------------------------
   main <- reactive({
+    ##Store trsList() as trsList (purely for easier interactive debugging)
+    trsList <- trsList()
+    
     ##Always-displayed headings
     stepHeads <- list(tiers = h2("Step 1: Validating tier names and attributes...",
                                  id="tierHead"),
@@ -763,15 +765,15 @@ server <- function(input, output) {
       ##By default, don't display
       undisplay()
     
-    ##Convenience: store rV$trsList as trsList
-    trsList <- rV$trsList
+    
     
     ##If not exiting early yet, check for tier issues
     if (!exitEarly || overrideExit$fileName) {
       ##Get tier issues
       tierIss <- trsList %>%
         imap(tierIssuesOneFile, 
-             nonSpkrTiers=nonSpkrTiers, prohibTiers=prohibTiers) %>% 
+             nonSpkrTiers=nonSpkrTiers, addlReqTiers=addlReqTiers,
+             prohibTiers=prohibTiers) %>% 
         keep(~ length(.x) > 0)
       ##If no tier issues, don't display anything & make step heading green
       if (length(tierIss)==0) {
@@ -991,15 +993,29 @@ server <- function(input, output) {
     if (exitEarly) {
       outdata <- list()
     } else {
-      ##Convert Praat seconds to Elan milliseconds
-      trsListFixed <- trsListFixed %>% 
-        map_if(~ inherits(.x, "trs_from_textgrid"),
-               ~ convert_times(.x, from="s", to="ms"))
-      
-      ##Change file extensions to .eaf
-      names(trsListFixed) <- names(trsListFixed) %>% 
-        file_path_sans_ext() %>% 
-        paste0(".eaf")
+      ##Convert Praat to Elan, if needed
+      anyTG <- 
+        trsListFixed %>% 
+        map_lgl(~ inherits(.x, "trs_from_textgrid")) %>% 
+        any()
+      if (anyTG) {
+        trsListFixed <- trsListFixed %>%
+          map_if(~ inherits(.x, "trs_from_textgrid"),
+                 ~ .x %>% 
+                   ##Convert Praat seconds to Elan milliseconds
+                   convert_times(from="s", to="ms") %>% 
+                   ##Change Transcriber tier text to AUTHOR attribute
+                   add_attributes(list(AUTHOR = .x %>% 
+                                         pluck("Transcriber", "Text") %>% 
+                                         unique())) %>% 
+                   remove_tiers("Transcriber")
+          )
+        
+        ##Change file extensions to .eaf
+        names(trsListFixed) <- names(trsListFixed) %>% 
+          file_path_sans_ext() %>% 
+          paste0(".eaf")
+      }
       
       ##Convert to trs_eaf
       outdata <- map(trsListFixed, trs_to_eaf)

eaf-utils.R
@@ -34,7 +34,7 @@ parse_filenames <- function(x,
          FileExt = file_ext(name))
   if (!is.null(readHandlers)) {
     out <- out %>% 
-      mutate(ReadHandler = map(tolower(FileExt), ~ pluck(readHandlers, .x)))
+      mutate(ReadHandler = map(tolower(FileExt), ~ readHandlers[[.x]]))
   }
 }
 
@@ -299,6 +299,43 @@ add_annotation_ids <- function(x, overwrite=c("error","warn","silent")) {
   attributes(x) <- xAttr
   
   x
+}
+
+##Remove tier(s) from trs_transcription (while preserving attributes)
+remove_tiers <- function(x, tiers=NULL, notier=c("error","warn","silent")) {
+  ##Check args
+  if (!inherits(x, c("trs_transcription", "trs_nesttiers"))) {
+    stop("x must be an object of classes trs_transcription and trs_nesttiers")
+  }
+  if (is.null(tiers) || !is.character(tiers)) {
+    stop("tiers must be a character vector")
+  }
+  notier <- match.arg(notier)
+  
+  ##Account for tiers not in x
+  missingTiers <- setdiff(tiers, names(x))
+  if (notier != "silent" && length(missingTiers) > 1) {
+    msg <- paste("Some tiers are missing from x:",
+                 paste(missingTiers, collapse=" "))
+    if (notier=="error") {
+      stop(msg)
+    } 
+    if (notier=="warn") {
+      if (identical(tiers, missingTiers)) {
+        stop("All tiers are missing from x")
+      }
+      warning(msg, "\nOnly removing tiers ",
+              paste(setdiff(tiers, missingTiers), collapse=" "))
+    }
+  }
+  
+  ##Remove tier(s) (even if they don't exist)
+  out <- x
+  for (tier in tiers) {
+    out[[tier]] <- NULL
+  }
+  
+  out
 }
 
 ##Convert a trs_transcription to an xml_document suitable for opening in Elan

commit ac3adbd78e8043dab8eb007c08d9609f6abef684

Fix heading-display issue introduced in 68dcaad; Correct shinytest for new dictSubhead

app.R
@@ -16,7 +16,8 @@ source("trs-utils.R")
 ##Version
 vers <- "1.4.2"
 
-##File structures
+## File structures ============================================================
+
 ##Named list of functions to handle each file extension to be read
 readHandlers <- list(eaf = xml2::read_xml,
                      ##read_textgrid() defined in trs-utils.R, plus args
@@ -680,9 +681,6 @@ server <- function(input, output) {
   
   # Main execution block ----------------------------------------------------
   main <- reactive({
-    ##Store trsList() as trsList (purely for easier interactive debugging)
-    trsList <- trsList()
-    
     ##Always-displayed headings
     stepHeads <- list(tiers = h2("Step 1: Validating tier names and attributes...",
                                  id="tierHead"),
@@ -702,6 +700,8 @@ server <- function(input, output) {
     ##If uploaded files, initialize exitEarly sentinel & proceed to checking
     ##  steps
     exitEarly <- FALSE
+    ##Store trsList() as trsList (purely for easier interactive debugging)
+    trsList <- trsList()
     
     
     ## Step 0: File check =====================================================
@@ -723,7 +723,7 @@ server <- function(input, output) {
     ##Style headings based on whether filenames are valid
     fileCheckValid <- all(validationDF$Valid)
     if (fileCheckValid) {
-      fileCheckSubhead <- h3("", id="fileCheckSubhead") %>% 
+      fileCheckSubhead <- h3("", id="fileCheckSubhead", class="subhead") %>% 
         undisplay()
       message("Step 0: Pass")
     } else {
@@ -747,7 +747,7 @@ server <- function(input, output) {
       fileCheckSubhead <- h3(paste0("Files must ", 
                                     str_flatten_comma(fileNameTips, ", and "),
                                     "."), 
-                            id="fileCheckSubhead") %>% 
+                            id="fileCheckSubhead", class="subhead") %>% 
         display()
       
       ##Edge case: Throw error if !fileCheckValid but no fileNameTips
@@ -761,8 +761,8 @@ server <- function(input, output) {
     
     # Step 1: Tier check ------------------------------------------------------
     ##Content
-    tierSubhead <- h3(paste("The tier checker returned the following issue(s):"),
-                      id="tierSubhead") %>% 
+    tierSubhead <- h3("The tier checker returned the following issue(s):",
+                      id="tierSubhead", class="subhead") %>% 
       ##By default, don't display
       undisplay()
     tierDetails <- tags$ul("", id="tierDetails", class="details") %>% 
@@ -819,7 +819,8 @@ server <- function(input, output) {
     
     # Step 2: Dictionary check ------------------------------------------------
     ##Content outsourced to dict-subhead.html because it's too long
-    dictSubhead <- div(includeHTML("misc/dict-subhead.html")) %>% 
+    dictSubhead <- div(includeHTML("misc/dict-subhead.html"),
+                       id="dictSubhead", class="subhead") %>% 
       ##By default, don't display
       undisplay()
     dictDetails <- tags$ul("", id="dictDetails", class="details") %>% 
@@ -886,7 +887,7 @@ server <- function(input, output) {
     overlapsSubhead <- h3(paste("The overlap checker could not resolve the following overlaps.",
                                 "Please fix these overlaps; remember to make overlaps a",
                                 "separate turn on each speaker's tier."),
-                          id="overlapsSubhead") %>% 
+                          id="overlapsSubhead", class="subhead") %>% 
       ##By default, don't display
       undisplay()
     overlapsDetails <- tags$ul("", id="overlapsDetails", class="details") %>% 
@@ -984,7 +985,7 @@ server <- function(input, output) {
     downloadHead <- h1("The file(s) passed all checks. Great job!",
                        id="downloadHead")
     downloadSubhead <- h3("Please download the corrected file(s) and upload to the Completed folder",
-                          id="downloadSubhead")
+                          id="downloadSubhead", class="subhead")
     downloadBtn <- downloadButton("OutputFile", "Download corrected file(s)")
     
     # UI tags ---------------------------------------------------------------

misc/dict-subhead.html
@@ -1,4 +1,4 @@
-<h3 id="dictSubhead">
+<h3>
   The following word(s) are not currently in the corpus dictionary.
   These could be:
   <ul>

tests/shinytest/shinytest-utils.R
@@ -57,7 +57,7 @@ snap <- function(app, name=NULL, path=getwd(), incl_eaflist=FALSE) {
     class = html_attr(html_elements(mainUI, "h2"), "class"),
     displayed = map_lgl(html_elements(mainUI, "h2"), is.displayed)
   )
-  stepSubheads <- html_elements(mainUI, "h3") %>% 
+  stepSubheads <- html_elements(mainUI, ".subhead") %>% 
     {data.frame(id = html_attr(., "id"),
                 displayed = map_lgl(., is.displayed))}
   success <- is.displayed(html_element(mainUI, "#downloadSubhead"))